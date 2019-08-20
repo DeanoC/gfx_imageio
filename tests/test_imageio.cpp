18dc58dd7595471308ef89bda231bdf05f02373d
deano <me@deanoc.com>
2019-08-20 05:06:21 +0300

    test fix

--- tests/test_imageio.cpp
+++ tests/test_imageio.cpp
@@ -1,6 +1,6 @@
 #include "al2o3_platform/platform.h"
 #include "gfx_image/image.h"
-#include "tiny_imageformat/tinyimageformat.h"
+#include "tiny_imageformat/tinyimageformat_base.h"
 #include "gfx_imageio/io.h"
 #include "al2o3_os/filesystem.h"
 #include "al2o3_vfile/vfile.hpp"
